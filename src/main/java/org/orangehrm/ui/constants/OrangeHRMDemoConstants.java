c8001f5b9c2b20da756c0a553bed7e0955c9b3a0
aniket1702 <aniketmaurya1702@gmail.com>
2022-12-17 23:41:19 +0530

    Added Log4j properties to log details to log file.

--- src/main/java/org/orangehrm/ui/constants/OrangeHRMDemoConstants.java
+++ src/main/java/org/orangehrm/ui/constants/OrangeHRMDemoConstants.java
@@ -8,8 +8,10 @@ public final class OrangeHRMDemoConstants {
     private static final String BASE_DIRECTORY=System.getProperty("user.dir");
     private static final String BROWSER= ConfigFactory.getConfig().browser();
     private static final String URL=ConfigFactory.getConfig().baseURL();
+    private static final String LOG4J_PATH=BASE_DIRECTORY+"/src/main/resources/properties/log4j.properties";
 
 
     public static String getBrowser(){return BROWSER;}
     public static String getURL(){return URL;}
+    public static String getLog4jPath(){return LOG4J_PATH;}
 }
